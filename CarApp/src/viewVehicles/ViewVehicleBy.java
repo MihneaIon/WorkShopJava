969235c0b5a55974028d26478c97b450b597d60f
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-07-28 11:29:43 +0300

    Create ViewVehicleBy.java

---
+++ CarApp/src/viewVehicles/ViewVehicleBy.java
@@ -0,0 +1,126 @@
+package viewVehicles;
+
+import interfaces.VehiclesModel;
+import models.Type;
+import models.Vehicle;
+import read.ReadFromFile;
+
+import java.util.*;
+
+public class ViewVehiclesBy implements VehiclesModel {
+
+    List<Vehicle> myVehicles;
+    private static final Scanner SCANNER = new Scanner(System.in);
+
+    @Override
+    public int compare(Vehicle o1, Vehicle o2) {
+        return 0;
+    }
+
+    @Override
+    public List<Vehicle> sortedVehiclesType(Type myType) {
+
+        List<Vehicle> printVehicles = new ArrayList<>();
+
+        try {
+            myVehicles = ReadFromFile.read();
+            System.out.println(myVehicles);
+            for (Iterator<Vehicle> iterator = myVehicles.iterator(); iterator.hasNext(); ) {
+                Vehicle value = iterator.next();
+                if (value.getType().equals(myType)) {
+                    printVehicles.add(value);
+                    iterator.remove();
+                }
+            }
+            printVehicles.addAll(myVehicles);
+            System.out.println(printVehicles);
+
+        } catch (Exception e) {
+            System.out.println("Error");
+            e.printStackTrace();
+        }
+
+        return printVehicles;
+    }
+
+
+    @Override
+    public List<Vehicle> sortedVehiclesBy(String choose) {
+        List<Vehicle> listOfPropertiesWanted = new ArrayList<>();
+        try {
+
+            listOfPropertiesWanted = ReadFromFile.read();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        if (choose.toLowerCase().equals("brand")) {
+
+            Collections.sort(listOfPropertiesWanted, (Vehicle v1, Vehicle v2) -> v1.getBrand().compareTo(v2.getBrand()));
+            for (Vehicle auxVehicles : listOfPropertiesWanted) {
+                System.out.println(auxVehicles);
+            }
+            //listOfPropertiesWanted = displayVehiclesByBrand();
+
+        } else if (choose.toLowerCase().equals("manufacturing year")) {
+
+            Collections.sort(listOfPropertiesWanted, (Vehicle v1, Vehicle v2) -> v1.getManufacturingYear() - v2.getManufacturingYear());
+            for (Vehicle auxVehicles : listOfPropertiesWanted) {
+                System.out.println(auxVehicles);
+            }
+            //listOfPropertiesWanted=displayVehiclesByManufacturingYear();
+        }
+
+        return listOfPropertiesWanted;
+    }
+
+    @Override
+    public List<Vehicle> viewAllVehicles() {
+        try {
+
+            myVehicles = ReadFromFile.read();
+            for (Vehicle auxVehicle : myVehicles) {
+                System.out.println(auxVehicle);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return myVehicles;
+    }
+
+    public void display() {
+
+    }
+
+//    class CompareAboutBrand implements Comparator<Vehicle>
+//    {
+//        @Override
+//        public int compare(Vehicle o1, Vehicle o2) {
+//            if (o1.getBrand().equals(o2.getBrand()))
+//                return 0;
+//            else if (o1.getBrand().hashCode() > o2.getBrand().hashCode())
+//                return 1;
+//
+//            return -1;
+//        }
+//    }
+//
+//    class CompareAboutYear implements Comparator<Vehicle>
+//    {
+//        @Override
+//        public int compare(Vehicle o1, Vehicle o2) {
+//            if(o1.getManufacturingYear()==o2.getManufacturingYear())
+//                return 0;
+//            else if(o1.getManufacturingYear()< o2.getManufacturingYear())
+//                return -1;
+//            return 1;
+//        }
+//    }
+
+
+}
+
+
