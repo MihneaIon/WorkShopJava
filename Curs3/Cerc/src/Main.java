2b79e181cb295e2fb2ac50b7fe4341a3622e33f9
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-07-28 09:34:52 +0300

    Update Main.java

--- Curs3/Cerc/src/Main.java
+++ Curs3/Cerc/src/Main.java
@@ -1,1 +1,16 @@
 
+import Figura.GenereazaCercuri;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        GenereazaCercuri myGenerator=new GenereazaCercuri();
+        myGenerator.initializare();
+        myGenerator.display();
+        System.out.println();
+        myGenerator.figuraUtil();
+
+
+    }
+}
