d43369a2c4c08b578e057604f883d649d1e2637c
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-07-28 09:41:18 +0300

    Add files via upload

---
+++ Curs4/Lambda/Main4.java
@@ -0,0 +1,28 @@
+package Lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main4 {
+
+    public static void main(String[] args) {
+
+        List<String> words =Arrays.asList("aolo","iepure","zonaq","elan","macelar","submarin","elvetia","aer","cer");
+
+        List<String> result=Exercise3.allMatches(words, s->s.toUpperCase(), s -> s.length()<4 && s.contains("E"));
+        String[] stockAsArr=new String[result.size()];
+        stockAsArr=result.toArray(stockAsArr);
+
+        for(String s: stockAsArr)
+        {
+            System.out.println(s);
+        }
+        try {
+            System.out.println(result.get(0));
+        }catch (Exception e)
+        {
+            System.out.println(" I haven't a such element ");
+        }
+
+    }
+}
