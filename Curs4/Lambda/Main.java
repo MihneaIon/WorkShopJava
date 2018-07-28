f1a9578299a8242ab62fc8fc9a45d7d5ccd62b8b
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-07-28 09:41:56 +0300

    Update Main.java

--- Curs4/Lambda/Main.java
+++ Curs4/Lambda/Main.java
@@ -1,1 +1,29 @@
 
+package Lambda;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        StreamExemple exemple=new StreamExemple();
+        exemple.met1();
+
+
+        List<String> words =Arrays.asList("aa","aaa","baad","cal","atacat","daltonist","belea");
+
+
+        List<String> shortWords=Exercise1.allMatches(words, s -> s.length()<4);
+
+        List<String> wordsWithB=Exercise1.allMatches(words, s->s.contains("b"));
+
+        List<String> evenLengthWords=Exercise1.allMatches(words,(s->s.length()%2==0));
+
+        System.out.println(shortWords);
+        System.out.println(wordsWithB);
+        System.out.println(evenLengthWords);
+
+    }
+}
