473828f5c8282366c8b73d979a9d5e7824a4c38d
Ion Mihnea <38386852+MihneaIon@users.noreply.github.com>
2018-07-28 11:24:18 +0300

    Create Agreement.java

---
+++ CarApp/src/agreement/Agreement.java
@@ -0,0 +1,14 @@
+package agreement;
+
+import models.Person;
+import models.Vehicle;
+
+import java.util.List;
+
+public class Agreement {
+
+    List<Person> myListOfPersons;
+    List<Vehicle> myListOfVehicles;
+
+
+}
